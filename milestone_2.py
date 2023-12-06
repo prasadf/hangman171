04ac73981f63a977385bbc1cd7c584eef53b1473
Prasad Fernando <prasadf@yahoo.com>
2023-12-06 18:36:56 +0000

    Adds milestone_2.py file of the hangman project

--- milestone_2.py
+++ milestone_2.py
@@ -6,16 +6,16 @@ print(word_list)
 
 # function to choose a random friut from word_list
 def choice(word_list):
-    word = random.choice(word_list)
-    return word
+    selected_word = random.choice(word_list)
+    return selected_word
 
 print(f'Random choice fruit is: {choice(word_list)}')
 # user input single letter
-guess = input('Please enter a single letter: ')
+guess_letter = input('Please enter a single letter: ')
 # check whether the user input is validls
 
-if len(guess) == 1:
+if len(guess_letter) == 1:
     print('Good guess!')
 else:
     print('Oops! This is not a valid input.')
-print(guess)
+print(guess_letter)
